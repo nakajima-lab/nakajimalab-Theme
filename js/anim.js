b88ecd5d74d9a534d7ce40ee0f3e63b554614edb
svertkattere` <86580403+2024040@users.noreply.github.comm>
2023-01-17 07:30:32 +0900

    fix

--- js/anim.js
+++ js/anim.js
@@ -162,6 +162,8 @@ const jsSidebar = ".sns-list__item";
 const jsHead = ".header-logo";
 const jsContent=".article-wrap, .section-area-left";
 
+const tl = gsap.timeline();
+
 gsap.set(
     [jsloadlogo, jsHead, jsGlonav],
     {
@@ -202,79 +204,78 @@ gsap.set(
     }
 )
 
-const tl = gsap.timeline();
+    tl.to(
+        [jsloadlogo, jsBeat],
+        {
+            opacity:1,
+            y:0,
+            duration:0.8,
+            delay:1,
+        }
+    ).to(
+        [jsloadlogo],
+        {
+            opacity:0,
+            y:-50,
+            duration:0.8,
+            delay:1,
+        },
+        '+=1'
+    ).to(
+        [jsBeat],
+        {
+            opacity:0,
+            y:50,
+            duration:0.8,
+        },
+        '<'
+    ).to(
+        jsloadbg,
+        {
+            opacity:0,
+        }
+    )
+    .to(
+        [jsHead, jsContent],
+        {
+            opacity:1,
+            y:0,
+            ease:"power1.inOot"
+        }
+    ).to(
+        jsGlonav,
+        {
+            opacity:1,
+            y:0,
+            stagger:{
+                from:"start",
+                each:0.1,
+                ease:"expo.out",
+            }
+        },
+        '<'
+    ).to(
+        jsSidebar,
+        {
+            x:0,
+            opacity:1,
+            stagger:{
+                from:"start",
+                grid:[1,0],
+                each:0.1,
+                ease:"expo.out",
+            }
+        },
+        '<'
+    ).to(
+        jshum,
+        {
+            x:0,
+            delay:0.3,
+            opacity:1,
+        }
+    );
 
-tl.to(
-    [jsloadlogo, jsBeat],
-    {
-        opacity:1,
-        y:0,
-        duration:0.8,
-        delay:1,
-    }
-).to(
-    [jsloadlogo],
-    {
-        opacity:0,
-        y:-50,
-        duration:0.8,
-        delay:1,
-    },
-    '+=5'
-).to(
-    [jsBeat],
-    {
-        opacity:0,
-        y:50,
-        duration:0.8,
-    },
-    '<'
-).to(
-    jsloadbg,
-    {
-        opacity:0,
-    }
-)
-.to(
-    [jsHead, jsContent],
-    {
-        opacity:1,
-        y:0,
-        ease:"power1.inOot"
-    }
-).to(
-    jsGlonav,
-    {
-        opacity:1,
-        y:0,
-        stagger:{
-            from:"start",
-            each:0.1,
-            ease:"expo.out",
-        }
-    },
-    '<'
-).to(
-    jsSidebar,
-    {
-        x:0,
-        opacity:1,
-        stagger:{
-            from:"start",
-            grid:[1,0],
-            each:0.1,
-            ease:"expo.out",
-        }
-    },
-    '<'
-).to(
-    jshum,
-    {
-        x:0,
-        delay:0.3,
-        opacity:1,
-    }
-)
 
 // const Loadbg = "#splash";
 
