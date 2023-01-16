5336e202e77709b2e4014e4a4aa09f0ea2ce2153
svertkattere` <86580403+2024040@users.noreply.github.comm>
2023-01-17 04:23:11 +0900

    fix

--- js/btn.js
+++ js/btn.js
@@ -1,11 +1,11 @@
 $(".openbtn").click(function () {//ボタンがクリックされたら 
     $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
     $("#glo-nav-sp").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-    $('body').toggleClass('noscroll');
+    // $('body').toggleClass('noscroll');
 });
 
 $(".glo-nav__item").click(function () {//ナビゲーションのリンクがクリックされたら
     $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
     $("#glo-nav-sp").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去し
-    $('body').removeClass('noscroll');
+    // $('body').removeClass('noscroll');
 });
