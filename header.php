6daa8603083917982a29228f273326f28fce1885
svertkattere` <86580403+2024040@users.noreply.github.comm>
2023-02-05 20:11:02 +0900

    add quicklink script

--- header.php
+++ header.php
@@ -41,6 +41,11 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/nkajimaLogo2.png">
     <script src="<?php echo get_template_directory_uri(); ?>/js/snap.svg-min.js"></script>
+    <script>
+        window.addEventListener('load', () => {
+            quicklink.listen();
+        });
+    </script>
     <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
